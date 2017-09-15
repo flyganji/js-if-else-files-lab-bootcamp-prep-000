7099d86e284f6417a961bd43f65acc221c410df7
anthony lee <admantony@gmail.com>
2017-09-15 17:02:06 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,14 +1,14 @@
 var index = require("./index.js")
 
 if (index.name === "Susan") {
-  console.log("Expected: Susan, Received: "+index.name)
+  console.log ("Expected: Susan, Received: "+index.name)
 } else {
-  ("The name is incorrect")
+  console.log ("The name is incorrect")
 }
 
 
 } if (index.name === 70) {
-    console.log("Expected:70, Received: "+index.height)
+    console.log ("Expected:70, Received: "+index.height)
 } else {
-  console.log("The height is incorrect")
+    console.log ("The height is incorrect")
 }
