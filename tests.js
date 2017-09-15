e2982af736f1e2fb0ca2614bbe074cb38397495b
anthony lee <admantony@gmail.com>
2017-09-15 16:59:45 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -4,4 +4,4 @@ if (index.name === "Susan") {
   console.log("Expected: Susan, Received: "+index.name)
 } else if (index.name === 70) {
     console.log("Expected:70, Received: "+index.height)
-} else (if (index.name))
+} 
